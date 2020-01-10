ee955a6cbd91f5ad79ab83d1d786c010030f9f15
Durdona A <holisihlos@gmail.com>
2020-01-09 22:11:50 -0500

    adding some dependencies

---
+++ my-map/src/components/Map.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+    <div class="columns">
+      <h2 class="column has-background-success">Hellloooo</h2>
+      <h2 class="column has-background-warning">Heeeeee</h2>
+      <h2 class="column has-background-danger">Heeeeee</h2>
+    </div>
+    
+     <div id="mapid"></div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Map",
+  data (){
+      return {
+           
+      }
+  },
+  methods: {
+       
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
