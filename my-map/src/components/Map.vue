0ceb593bff8158ea70c2afeaf3b965beab4964a5
Durdona A <holisihlos@gmail.com>
2020-01-14 16:33:50 -0500

    added leaflet retrieved  data

--- my-map/src/components/Map.vue
+++ my-map/src/components/Map.vue
@@ -9,24 +9,66 @@
     </div>
     <div>
       <h1
-        class="title is-1 has-text-centered is-size-desktop has-text-black-bis"
+        class="title is-1 has-text-centered is-size-desktop has-text-black-bis "
       >
         Interactive Map
       </h1>
     </div>
-    <div id="mapid"></div>
+    <div class="columns">
+      <Forecast :forecast="forecast" />
+      <div id="mapid" class="column is-two-thirds">
+        <l-map :zoom="zoom" :center="center">
+          <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+          <l-marker
+            v-for="(forecasts, index) in forecast"
+            v-bind:key="index"
+            :lat-lng="latLng(forecasts.latitude, forecasts.longitude)"
+          ></l-marker>
+        </l-map>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import axios from "axios";
+import Forecast from "./Forecast.vue";
+
 export default {
   name: "Map",
   data() {
-    return {};
+    return {
+      forecast: [],
+      zoom: 2,
+      // eslint-disable-next-line no-undef
+      center: L.latLng(47.41322, -1.219482),
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      // eslint-disable-next-line no-undef
+      marker: L.latLng(47.41322, -1.219482)
+    };
   },
-  methods: {},
+  mounted: function() {
+    axios.get(" https://api.openbrewerydb.org/breweries").then(r => {
+      this.forecast = r.data;
+    });
+  },
+  methods: {
+    latLng: function(lat, lng) {
+      // eslint-disable-next-line no-undef
+      return L.latLng(lat, lng);
+    }
+  },
   props: {
     msg: String
+  },
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    Forecast
   }
 };
 </script>
@@ -34,6 +76,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .section {
-  padding: 0.5rem 0.5rem;
+  padding: 1rem 0.5rem;
+}
+h1 {
+  margin-bottom: 1rem;
+}
+#mapid {
+  height: 83vh;
 }
 </style>
